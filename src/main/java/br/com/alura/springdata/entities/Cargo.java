01faf954984a9993dbd95249bb8275697ac2cb81
leo-jansen <janseleo@gmail.com>
2021-08-06 10:09:00 -0300

    add entity cargo

---
+++ src/main/java/br/com/alura/springdata/entities/Cargo.java
@@ -0,0 +1,29 @@
+package br.com.alura.springdata.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String descricao;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Descrição do cargo: %s", this.descricao);
+  }
+}
