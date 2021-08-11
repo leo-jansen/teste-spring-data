f3d172954aa1ff6f51d61338ccc6b44bf733ef01
leo-jansen <janseleo@gmail.com>
2021-08-11 13:15:07 -0300

    inserir cago

--- src/main/java/br/com/alura/springdata/SpringDataApplication.java
+++ src/main/java/br/com/alura/springdata/SpringDataApplication.java
@@ -1,12 +1,27 @@
 package br.com.alura.springdata;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.springdata.entities.Cargo;
+import br.com.alura.springdata.repositories.CargoRepository;
+
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+	private final CargoRepository cargoRepository;
+
+	SpringDataApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo("Desenvolvedor de Software");
+		cargoRepository.save(cargo);
+	}
 }
