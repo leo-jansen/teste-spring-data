c4fefffb5205799009735144782264617695fdfe
leo-jansen <janseleo@gmail.com>
2021-09-13 00:38:16 -0300

    comentario, Native Query | JPQL | Derived Query

--- src/main/java/br/com/alura/springdata/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/alura/springdata/repositories/FuncionarioRepository.java
@@ -11,10 +11,18 @@ import br.com.alura.springdata.entities.Funcionario;
 
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
   List<Funcionario> findByNome(String nome); //Derived Query [https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods.query-creation]
-  /*List<Funcionario> findByNameAndSalarioGreaterThanAndDataContratacao(String nome, BigDecimal salario, LocalDate data);
-  List<Funcionario> findByCargoDescricao(String descricao);*/
+  // List<Funcionario> findByNameAndSalarioGreaterThanAndDataContratacao(String nome, BigDecimal salario, LocalDate data);
+  /** Derived Query */
+  List<Funcionario> findByCargoDescricao(String descricao);
+  
+  /** JPQL */
   @Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
   List<Funcionario> buscarNomeSalarioMaiorDataContratacao(String nome, BigDecimal salario, LocalDate data);
-  @Query("SELECT f FROM Funcionario f JOIN f.cargo c WHERE c.descricao = :descricao")
-  List<Funcionario> buscarCargoPelaDescricao(String descricao);
+  
+  /*@Query("SELECT f FROM Funcionario f JOIN f.cargo c WHERE c.descricao = :descricao")
+  List<Funcionario> buscarCargoPelaDescricao(String descricao);*/
+  
+  /** Native Query */
+  @Query(value = "SELECT * FROM funcionarios f WHERE f.data_contratacao >= :data", nativeQuery = true)
+  List<Funcionario> buscarDataContratacaoMaior(LocalDate data);
 }
--- src/main/java/br/com/alura/springdata/services/FuncionarioService.java
+++ src/main/java/br/com/alura/springdata/services/FuncionarioService.java
@@ -2,7 +2,6 @@ package br.com.alura.springdata.services;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
